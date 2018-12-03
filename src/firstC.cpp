8b55bacd3b34d6538659bb7805f88dff3bb0e663
yanup <yanup@windows10.microdone.cn>
2018-12-03 20:57:11 +0800

    初次提交

---
+++ src/firstC.cpp
@@ -0,0 +1,17 @@
+//============================================================================
+// Name        : firstC.cpp
+// Author      : upup
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+//	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	printf("hehehehehe\n");
+	printf("我是来学习c语言的呀");
+	return 0;
+}
