3dce63a1277388f87cc1e8f3fe3e8a008973f0f3
yanup <yanup@windows10.microdone.cn>
2018-12-26 22:01:39 +0800

    一维数组

--- src/firstC.cpp
+++ src/firstC.cpp
@@ -12,6 +12,25 @@ using namespace std;
 int main() {
 //	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
 	printf("hehehehehe\n");
+
 	printf("我是来学习c语言的呀");
+
+	int c = 0;
+	for(int a = 0; a<= 100; a++) {
+		c+=a;
+	}
+	printf("1+2+。。。+ 100的结果是：%d", c);
+
+	int putchar(int ch);
+	putchar('A');
+
+	int i, a[10];
+	for(i = 0; i<=9;i++){
+		a[i] = i;
+	}
+	printf("\n");
+	for(i = 9; i>= 0 ;i--) {
+		printf("%d\n", a[i]);
+	}
 	return 0;
 }
